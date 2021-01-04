c3005ca903035c7723264115206ffefb471b29c9
Cleber <rcleber@outlook.com.br>
2021-01-04 15:27:22 -0300

    criação da tabela execução.

--- anaplan/integrador/models.py
+++ anaplan/integrador/models.py
@@ -23,6 +23,7 @@ class Modelo(TimeStampedModel):
 
 class ProcessList(TimeStampedModel):
     descricao = models.CharField(max_length=80, unique=True)
+    nome_processo_anaplan = models.CharField(max_length=80, verbose_name="Nome Processo Anaplan")
     modelo = models.ForeignKey(Modelo, models.CASCADE, verbose_name="Modelo", related_name='modelo_processlist')
 
     def __str__(self):
@@ -45,6 +46,22 @@ class Processo(TimeStampedModel):
 
     def __str__(self):
         return self.nome
+
+
+class Execucao(TimeStampedModel):
+    descricao = models.CharField(max_length=80)
+    processlist = models.ForeignKey(ProcessList, models.CASCADE, verbose_name="Process List", related_name="execucao_processlist")
+ 
+    def __str__(self):
+        return self.descricao
+
+class ParametrosExecucao(TimeStampedModel):
+    execucao = models.ForeignKey(Execucao, models.CASCADE, verbose_name="Execução", related_name="parametrosexecucao_execucao")
+    arquivo_anaplan = models.CharField(max_length=50)
+    arquivo_forno = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.arquivo_anaplan
 
 
 class Historico(TimeStampedModel):
